7b942fffdfd8d84c6449f9e4fdd5daf90e520c8a
foe <foefabrice@gmail.com>
2016-11-30 17:33:28 +0100

    création de l'entité recevoir et de son repository

---
+++ src/GestionPersonelBundle/Entity/recevoir.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace GestionPersonelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * recevoir
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GestionPersonelBundle\Entity\recevoirRepository")
+ */
+class recevoir
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="motif", type="string", length=255)
+     */
+    private $motif;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return recevoir
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set motif
+     *
+     * @param string $motif
+     * @return recevoir
+     */
+    public function setMotif($motif)
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    /**
+     * Get motif
+     *
+     * @return string 
+     */
+    public function getMotif()
+    {
+        return $this->motif;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return recevoir
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
