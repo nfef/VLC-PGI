2d4fa9a5b1e61cdcc180481195fc95d84c561d6a
foe <foefabrice@gmail.com>
2016-11-30 17:10:07 +0100

    création de l'entité sanctions et de son repository, puis modification du type du champ dateentre dans l'entité contrat

--- src/GestionPersonelBundle/Entity/contrat.php
+++ src/GestionPersonelBundle/Entity/contrat.php
@@ -31,7 +31,7 @@ class contrat
     /**
      * @var string
      *
-     * @ORM\Column(name="dateentree", type="string", length=255)
+     * @ORM\Column(name="dateentree", type="date")
      */
     private $dateentree;
 
---
+++ src/GestionPersonelBundle/Entity/sanctions.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace GestionPersonelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * sanctions
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GestionPersonelBundle\Entity\sanctionsRepository")
+ */
+class sanctions
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="motif", type="string", length=255)
+     */
+    private $motif;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return sanctions
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set motif
+     *
+     * @param string $motif
+     * @return sanctions
+     */
+    public function setMotif($motif)
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    /**
+     * Get motif
+     *
+     * @return string 
+     */
+    public function getMotif()
+    {
+        return $this->motif;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return sanctions
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
