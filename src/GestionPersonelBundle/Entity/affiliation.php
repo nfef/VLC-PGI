52ab242f28c6ffff885045f7005c4f522144d1c9
foe <foefabrice@gmail.com>
2016-11-30 17:33:06 +0100

    création de l'entité affiliation et de son repository

---
+++ src/GestionPersonelBundle/Entity/affiliation.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace GestionPersonelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * affiliation
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GestionPersonelBundle\Entity\affiliationRepository")
+ */
+class affiliation
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="y", type="string", length=255)
+     */
+    private $y;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return affiliation
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * Set y
+     *
+     * @param string $y
+     * @return affiliation
+     */
+    public function setY($y)
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get y
+     *
+     * @return string 
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+}
