b3d60c01181847ce76a5234f48732af0d9e494db
foe <foefabrice@gmail.com>
2016-11-30 17:28:34 +0100

    création de l'entité type_indemnite et de son repository

---
+++ src/GestionPersonelBundle/Entity/type_indemnite.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace GestionPersonelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * type_indemnite
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GestionPersonelBundle\Entity\type_indemniteRepository")
+ */
+class type_indemnite
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return type_indemnite
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return type_indemnite
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
