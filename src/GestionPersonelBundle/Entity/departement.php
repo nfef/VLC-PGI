8fe4f529107619ba5ecc7e5b8dfb665c3e4424b7
foe <foefabrice@gmail.com>
2016-11-30 17:25:39 +0100

    création de l'entité departement et de son repository

---
+++ src/GestionPersonelBundle/Entity/departement.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace GestionPersonelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * departement
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GestionPersonelBundle\Entity\departementRepository")
+ */
+class departement
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255)
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return departement
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+}
